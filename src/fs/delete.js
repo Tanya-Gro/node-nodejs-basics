599e1da9338c23d6d8006d106d4fea8a42732a2a
Tanya-Gro <tarpel.gta@gmail.com>
2025-10-26 18:36:44 +0200

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,30 @@
+import { access, constants, unlink } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const DELETE_FILE = 'fileToRemove.txt';
+const ERR_MSG = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const deletePath = path.join(__dirname, 'files', DELETE_FILE);
+
 const remove = async () => {
-  // Write your code here
+  try {
+    await access(deletePath, constants.F_OK);
+  } catch (err) {
+    console.error(`Error: file ${DELETE_FILE} doesn't exist`);
+    throw new Error(ERR_MSG);
+  }
+
+  try {
+    await unlink(deletePath);
+    console.log('File successfully deleted');
+  } catch (err) {
+    console.error('Unexpected FS error');
+    throw new Error(ERR_MSG);
+  }
 };
 
 await remove();
