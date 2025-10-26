8176072e891dc49e23ce75cac9e4db2061e13e46
Tanya-Gro <tarpel.gta@gmail.com>
2025-10-26 18:17:21 +0200

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,42 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const OLD_FILE = 'wrongFilename.txt';
+const NEW_FILE = 'properFilename.md';
+const ERR_MSG = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.join(path.dirname(__filename), 'files');
+
+const oldPath = path.join(__dirname, OLD_FILE);
+const newPath = path.join(__dirname, NEW_FILE);
+
 const rename = async () => {
-  // Write your code here
-};
+  try {
+    await fs.access(oldPath, fs.constants.F_OK);
+  } catch (err) {
+  console.error(`Error: file ${OLD_FILE} doesn't exist`);
+  throw new Error(ERR_MSG);
+  }
+
+  try {
+    await fs.access(newPath, fs.constants.F_OK);
+    console.error(`Error: file ${NEW_FILE} exist`);
+    throw new Error(ERR_MSG);
+  } catch (err) {
+    if (err.message === ERR_MSG) {
+      throw err;
+    }
+  }
+
+  try {
+    await fs.rename(oldPath, newPath);
+    console.log('File renamed successfully.');
+  } catch (err) {
+    console.error('Unexpected FS error');
+    throw new Error(ERR_MSG);
+  }
+}
 
 await rename();
