7b397ba44c200f55b0e64afe8c8b107096bb9b20
Tanya-Gro <tarpel.gta@gmail.com>
2025-10-26 20:04:50 +0200

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,38 @@
+import { stat, readdir } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const SOURCE_FOLDER = 'files';
+const ERR_MSG = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sourcePath = path.join(__dirname, SOURCE_FOLDER);
+
 const list = async () => {
-  // Write your code here
+  try {
+    const stats = await stat(sourcePath); 
+    if (!stats.isDirectory()) { 
+      throw new Error(ERR_MSG);
+    }
+  } catch (err) {
+    console.error(`Error: folder "${SOURCE_FOLDER}" doesn't exist`);
+    throw new Error(ERR_MSG);
+  }
+
+  try {
+    const files = await readdir(sourcePath); 
+    if (files.length > 0) {
+      console.log(`Files in "${SOURCE_FOLDER}":`);
+      files.forEach((file, index) => console.log(`${index + 1}) ${file}`));
+    } else {
+      console.log(`There are no files in "${SOURCE_FOLDER}" folder`);
+    }
+  } catch (err) {
+    console.error('Unexpected FS error');
+    throw new Error(ERR_MSG);
+  }
 };
 
 await list();
