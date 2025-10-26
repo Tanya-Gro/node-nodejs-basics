82b6ce6420083d2a4f20cf92dff68b3252f8af72
Tanya-Gro <tarpel.gta@gmail.com>
2025-10-26 20:20:37 +0200

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,36 @@
+import { access, constants, readFile } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const READ_FILE = 'fileToRead.txt';
+const ERR_MSG = 'FS operation failed';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const readPath = path.join(__dirname, 'files', READ_FILE);
+
 const read = async () => {
-  // Write your code here
+  try {
+    await access(readPath, constants.F_OK);
+  } catch (err) {
+    console.error(`Error: file ${READ_FILE} doesn't exist`);
+    throw new Error(ERR_MSG);
+  }
+
+  try {
+    const contents = await readFile(readPath, { encoding: 'utf8' });
+    if (contents) {
+      console.log('-= File content: =-');
+      console.log(contents);
+      console.log('-= The END =-');
+    } else {
+      console.log(`"${READ_FILE}" file is empty`);
+    }
+  } catch (err) {
+    console.error('Unexpected FS error');
+    throw new Error(ERR_MSG);
+  }
 };
 
 await read();
