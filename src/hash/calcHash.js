6b744a57273783f611254b483dcd992286e031dc
Tanya-Gro <tarpel.gta@gmail.com>
2025-10-26 23:25:49 +0200

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import { pipeline } from 'node:stream/promises';
+
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CALCULATE_FILE = 'fileToCalculateHashFor.txt';
+const calculatePath = path.join(__dirname, 'files', CALCULATE_FILE);
+const ERR_MSG = 'FS operation failed';
+
 const calculateHash = async () => {
-  // Write your code here
+  try {
+    const hash = createHash('sha256');
+    const input = createReadStream(calculatePath);
+
+    await pipeline(input, hash);
+
+    const result = hash.digest('hex');
+    console.log(result);
+
+    return result;
+  } catch(err) {
+    throw new Error(ERR_MSG); 
+  };
 };
 
 await calculateHash();
